f1ffb6d490e880ccf4228e66cdff6a12e3b29ed3
Charles <46288486+ItzFrostbiteTv@users.noreply.github.com>
2019-03-11 21:14:27 -0500

    Add files via upload

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 height = float(input("Enter Height In Inchs: "))
 lbs = float(input("Enter Weight In Pounds: "))
 
@@ -6,7 +8,13 @@ BMI = round(lbs / (height * height) * 703, 2)
 print("Your BMI Is:", BMI)
 if BMI <= 24.9 and BMI >= 18.5:
     print("You Are Healthy")
+    sleep(3)
+    exit = input("Press Enter To Exit: ")
 elif BMI > 24.9:
     print("You Are Overweight")
+    sleep(3)
+    exit = input("Press Enter To Exit: ")
 elif BMI < 18.5:
     print("You Are Underweight")
+    sleep(3)
+    exit = input("Press Enter To Exit: ")
